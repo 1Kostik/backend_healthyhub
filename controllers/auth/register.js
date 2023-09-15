22a802adb2ca1de6ec10e4d28a97d99b09548458
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-09-15 16:51:41 +0300

    up

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,4 +1,4 @@
-const crypto = require("node:crypto");
+const crypto = require("crypto");
 const bcrypt = require("bcrypt");
 const { BASE_URL } = process.env;
 const { User } = require("../../models");
