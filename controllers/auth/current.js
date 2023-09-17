d5f671d1dcdc5f08b1c0fcb5a25aa16f0c14ed32
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-09-17 13:49:40 +0300

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -5,41 +5,6 @@ require("dotenv").config();
 
 const { SECRET_KEY } = process.env;
 
-// const current = async (req, res) => {
-//   const token = req.headers.authorization?.split(" ")[1];
-//   if (!token) {
-//     return res.status(401).send({ message: "Unauthorized" });
-//   }
-
-//   try {
-//     const decoded = jwt.verify(token, SECRET_KEY);
-//     const user = await User.findById(decoded.id);
-
-//     if (!user) {
-//       return res.status(404).send({ message: "User not found" });
-//     }
-
-//     res.json({
-//       status: "success",
-//       code: 200,
-//       user: {
-//         name: user.name,
-//         email: user.email,
-//         goal: user.goal,
-//         gender: user.gender,
-//         age: user.age,
-//         height: user.height,
-//         weight: user.weight,
-//         activity: user.activity,
-//       },
-//     });
-//   } catch (error) {
-//     res.status(401).send({ message: "Unauthorized" });
-//   }
-// };
-
-// module.exports = current;
-
 const current = async (req, res) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
