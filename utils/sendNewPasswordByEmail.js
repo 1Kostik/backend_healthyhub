6afa641a1d555d53d1c5a5229f90ef7fa78fc479
1Kostik <117483719+1Kostik@users.noreply.github.com>
2023-09-18 01:03:39 +0300

    update email verification
    all work

--- utils/sendNewPasswordByEmail.js
+++ utils/sendNewPasswordByEmail.js
@@ -2,23 +2,24 @@ const nodemailer = require("nodemailer");
 
 async function sendNewPasswordByEmail(email, newPassword) {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
+    const transport = nodemailer.createTransport({
+      host: 'smtp.meta.ua',
+      port: 465,
+      secure: true,
       auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASSWORD,
+        user: process.env.META_USER,
+        pass: process.env.META_PASSWORD,
       },
     });
 
     const mailOptions = {
-      from: "sorochankostya@gmail.com",
+      from: "sorochnkostya@meta.ua",
       to: email,
       subject: "Password Reset",
       text: `Your new password is: ${newPassword}`,
     };
 
-    await transporter.sendMail(mailOptions);
+    await transport.sendMail(mailOptions);
 
     console.log("Password reset email sent successfully");
   } catch (error) {
