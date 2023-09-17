7a79041f05e4c5eab408dd7081a0b76e137cf148
VitalyOvsiyenko <ovsiyenko80@gmail.com>
2023-09-17 11:26:54 +0300

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,8 +1,36 @@
+const jwt = require("jsonwebtoken");
 const { User } = require("../../models");
-const { HttpError } = require("../../utils");
+
+require("dotenv").config();
+
+const { SECRET_KEY } = process.env;
 
 const current = async (req, res) => {
-  const { email, subscription } = req.user;
-  res.status(200).json({ email, subscription });
+  const token = req.headers.authorization?.split(" ")[1];
+  if (!token) {
+    return res.status(401).send({ message: "Unauthorized" });
+  }
+
+  try {
+    const decoded = jwt.verify(token, SECRET_KEY);
+    const user = await User.findById(decoded.id);
+
+    if (!user) {
+      return res.status(404).send({ message: "User not found" });
+    }
+
+    res.json({
+      status: "success",
+      code: 200,
+      user: {
+        email: user.email,
+        name: user.name,
+        goal: user.goal,
+      },
+    });
+  } catch (error) {
+    res.status(401).send({ message: "Unauthorized" });
+  }
 };
+
 module.exports = current;
