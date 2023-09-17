2b8c5c8bc70b9369be9404f301a2188f3b20576a
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-09-18 00:02:47 +0300

    added sengrid sending

--- utils/sendNewPasswordByEmail.js
+++ utils/sendNewPasswordByEmail.js
@@ -1,24 +1,16 @@
-const nodemailer = require("nodemailer");
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 async function sendNewPasswordByEmail(email, newPassword) {
   try {
-    const transporter = nodemailer.createTransport({
-      host: "sandbox.smtp.mailtrap.io",
-      port: 2525,
-      auth: {
-        user: process.env.MAILTRAP_USER,
-        pass: process.env.MAILTRAP_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: "sorochankostya@gmail.com",
+    const msg = {
       to: email,
-      subject: "Password Reset",
+      from: 'petro.shutak.ua@gmail.com', 
+      subject: 'Password Reset',
       text: `Your new password is: ${newPassword}`,
     };
 
-    await transporter.sendMail(mailOptions);
+    await sgMail.send(msg);
 
     console.log("Password reset email sent successfully");
   } catch (error) {
@@ -30,3 +22,36 @@ async function sendNewPasswordByEmail(email, newPassword) {
 module.exports = {
   sendNewPasswordByEmail,
 };
+
+// const nodemailer = require("nodemailer");
+
+// async function sendNewPasswordByEmail(email, newPassword) {
+//   try {
+//     const transporter = nodemailer.createTransport({
+//       host: "sandbox.smtp.mailtrap.io",
+//       port: 2525,
+//       auth: {
+//         user: process.env.MAILTRAP_USER,
+//         pass: process.env.MAILTRAP_PASSWORD,
+//       },
+//     });
+
+//     const mailOptions = {
+//       from: "sorochankostya@gmail.com",
+//       to: email,
+//       subject: "Password Reset",
+//       text: `Your new password is: ${newPassword}`,
+//     };
+
+//     await transporter.sendMail(mailOptions);
+
+//     console.log("Password reset email sent successfully");
+//   } catch (error) {
+//     console.error("Error sending password reset email:", error);
+//     throw error;
+//   }
+// }
+
+// module.exports = {
+//   sendNewPasswordByEmail,
+// };
