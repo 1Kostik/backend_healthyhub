2c30433a39ff24a143fe812f38b87fc3747aed79
Petro Shutak <113899587+PetroShutak@users.noreply.github.com>
2023-09-18 20:52:17 +0300

    fixed mailsending

--- utils/sendNewPasswordByEmail.js
+++ utils/sendNewPasswordByEmail.js
@@ -1,26 +1,30 @@
 const nodemailer = require("nodemailer");
 
-const transport = nodemailer.createTransport({
-  host: "smtp.meta.ua",
-  port: 465,
-  secure: true,
-  auth: {
-    user: process.env.META_USER,
-    pass: process.env.META_PASSWORD,
-  },
-});
-function sendNewPasswordByEmail(email, newPassword) {
+async function sendNewPasswordByEmail(email, newPassword) {
+  try {
+    const transporter = nodemailer.createTransport({
+      host: "smtp.meta.ua",
+      port: 465,
+      secure: true,
+      auth: {
+        user: process.env.META_USER,
+        pass: process.env.META_PASSWORD,
+      },
+    });
 
     const mailOptions = {
       from: "sorochnkostya@meta.ua",
       to: email,
       subject: "Password Reset",
-      html: `<p>To confirm your registration, please click on the link below:\n
-      ${newPassword}"</p>`,     
+      text: `Your new password is: ${newPassword}`,
+      html: `<p>Your new password is:\n ${newPassword}</p>`,
     };
 
-    return transport.sendMail(mailOptions);     
- 
+    await transporter.sendMail(mailOptions);
+  } catch (error) {
+    console.error("Error sending password reset email:", error);
+    throw error;
+  }
 }
 
 module.exports = {
