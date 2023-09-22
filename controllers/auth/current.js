20fc714f21151590e5a0fc2a9264ea97bfd90165
Alex <117479029+AlexTkachenkoFullstack@users.noreply.github.com>
2023-09-22 16:13:29 +0100

    Update current.js

--- controllers/auth/current.js
+++ controllers/auth/current.js
@@ -1,21 +1,5 @@
-const jwt = require("jsonwebtoken");
-const { User } = require("../../models");
-
-const { SECRET_KEY } = process.env;
-
 const current = async (req, res) => {
-  try {
-    const token = req.headers.authorization?.split(" ")[1];
-    if (!token) {
-      return res.status(401).json({ message: "Unauthorized" });
-    }
-
-    const decoded = jwt.verify(token, SECRET_KEY);
-    const user = await User.findById(decoded.id);
-
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
+  const user=req.user;
 
     res.status(200).json({
       status: "success",
@@ -30,10 +14,8 @@ const current = async (req, res) => {
         activity: user.activity,
         avatarURL: user.avatarURL,
       },
+      token:user.token
     });
-  } catch (error) {
-    res.status(401).json({ message: "Unauthorized" });
-  }
 };
 
 module.exports = current;
